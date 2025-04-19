72cd08fc764e281ea1869bf552b9a252c3c3d1f5
karthikeyan2424 <triplek627@gmail.com>
2025-04-19 11:23:20 +0530

    first

--- base/forms.py
+++ base/forms.py
@@ -12,13 +12,12 @@ class TaskForm(forms.ModelForm):
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
-            'dayin': DateTimeInput(
-                attrs={'type': 'datetime-local', 'class': 'form-control'},
-                format='%Y-%m-%dT%H:%M'
-            ),
+            'dayin': forms.DateInput(attrs={
+                'type': 'date',
+                'class': 'form-control'
+            }),
             'complete': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
         }
-
     def __init__(self, *args, **kwargs):
         super(TaskForm, self).__init__(*args, **kwargs)
         self.fields['dayin'].input_formats = ['%Y-%m-%dT%H:%M']
